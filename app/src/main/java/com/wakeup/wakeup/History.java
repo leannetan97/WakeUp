c2c699b098d8036c7e7944f7b18d3c08e39eb622
hsunwei <hectoryeeos@gmail.com>
2019-10-20 22:54:22 +0800

    add graph

--- app/src/main/java/com/wakeup/wakeup/History.java
+++ app/src/main/java/com/wakeup/wakeup/History.java
@@ -2,13 +2,54 @@ package com.wakeup.wakeup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class History extends AppCompatActivity {
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        barChart = findViewById(R.id.historyChart);
+
+        getEntries();
+
+        barDataSet = new BarDataSet(barEntries, "Minute");
+        barData = new BarData(barDataSet);
+
+
+        barChart.setData(barData);
+
     }
+
+    private void getEntries() {
+        barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 4f));
+        barEntries.add(new BarEntry(2, 3f));
+        barEntries.add(new BarEntry(3,8f));
+        barEntries.add(new BarEntry(4, 6f));
+        barEntries.add(new BarEntry(5, 9f));
+        barEntries.add(new BarEntry(6, 10f));
+        barEntries.add(new BarEntry(7, 6f));
+    }
+
+
+
+
 }
--- app/src/main/java/com/wakeup/wakeup/MainActivity.java
+++ app/src/main/java/com/wakeup/wakeup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wakeup.wakeup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.wakeup.wakeup.ui.main.SectionsPagerAdapter;
 
@@ -33,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        // History
+        Button btnHistory = (Button)findViewById(R.id.btn_history);
+        btnHistory.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, History.class);
+                startActivity(I);
             }
         });
     }
