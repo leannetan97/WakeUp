ffaf153c77f85f742eb47202b98c59d23f3ac2f5
Xun Wei Yee <hectoryeeos@gmail.com>
2019-11-17 13:18:34 +0800

    add User class, test on firebase write

--- app/src/main/java/com/wakeup/wakeup/Login.java
+++ app/src/main/java/com/wakeup/wakeup/Login.java
@@ -1,5 +1,6 @@
 package com.wakeup.wakeup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,20 +8,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
+//    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+//    DatabaseReference mconditionRef = mRootRef.child("condition");
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("users");
+
+    User user = new User("mnamepri", "mpw");
+
+//    private DatabaseReference mDatabase;
+//    mDatabase = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+//        mconditionRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String text = dataSnapshot.getValue(String.class);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        })
     }
 
     public void click(View v)
     {
-        Intent GoToRegPage = new Intent(this, SignUp.class);
-        startActivity(GoToRegPage);
+//        // Write a message to the database
+
+        myRef = myRef.child("user2"); //.child("mname");
+        myRef.setValue(user);
+
+//        Intent GoToRegPage = new Intent(this, SignUp.class);
+//        startActivity(GoToRegPage);
     }
 
     public void alarm(View v)
